e46b4bcc5c88cc2553da56f77efec0b5851de2b0
Stupin Sergey <stupin.sergey@gmail.com>
2012-07-10 00:55:33 +0400

    Init project with Intellij idea project files

---
+++ src/clj_pymorphy/core.clj
@@ -0,0 +1,8 @@
+(ns clj-pymorphy.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
+
+(-main)
---
+++ test/clj_pymorphy/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-pymorphy.core-test
+  (:use clojure.test
+        clj-pymorphy.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
