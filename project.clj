d0620c1efc218333cd1f56f5f23db8647f5a98ac
Sergey <stupin.sergey@gmail.com>
2012-08-13 11:52:41 +0400

    Fix url tag

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-pymorphy "0.1.0-SNAPSHOT"
   :description "Clojure-pymorphy wraps the main functionality of the python pymorphy library.
   For more information look at official pymorphy page http://packages.python.org/pymorphy/"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/hsestupin/clojure-pymorphy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
